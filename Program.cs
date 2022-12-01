25111343a4fb8be9618eca30c4b73d682a802f3a
dynamics <31436988+dynamics1@users.noreply.github.com>
2022-12-01 23:47:32 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public static class Globals
+{
+	internal static void Main()
+	{
+		int n;
+		("%d", n) = Convert.ToInt32(value: Console.ReadLine());
+		int dir = 1;
+		int a;
+		int b;
+		for (int i = 0; i < n; i++)
+		{
+			("%d", a) = Convert.ToInt32(value: Console.ReadLine());
+			if (dir != 0)
+			{
+				if (i != 0 && b > a)
+				{
+					dir = 0;
+				}
+			}
+			else
+			{
+				if (b < a)
+				{
+					b = a;
+				}
+			}
+		}
+		return 1;
+	}
+}
+
